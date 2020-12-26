aab6e88f2c4cbfd842f848b5ea255a56decd07b6
macarc <archiepiper@hotmail.com>
2020-12-26 18:24:18 +0000

    Remove unused files

--- uhtmlscript.ts
+++
@@ -1,7 +0,0 @@
-/*import { render } from 'uhtml';
-import Score, { ScoreModel } from './Score';
-
-const updateScore = (newScore: ScoreModel) => render(document.body, Score.render(newScore, { updateScore }));
-
-updateScore(Score.init());
-*/
