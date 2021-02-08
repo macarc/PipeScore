e46083fb49200e4ef2c6c1d3a0bf1c4f55000e7f
macarc <archiepiper@hotmail.com>
2021-02-08 16:57:30 +0000

    Remove .js files, update package.json

--- src/Bar/model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Barline = void 0;
-var Barline;
-(function (Barline) {
-    Barline[Barline["RepeatFirst"] = 0] = "RepeatFirst";
-    Barline[Barline["RepeatLast"] = 1] = "RepeatLast";
-    Barline[Barline["Normal"] = 2] = "Normal";
-})(Barline = exports.Barline || (exports.Barline = {}));
