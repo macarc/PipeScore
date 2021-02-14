0bcafa2457a520aecdc94cf29fef2f70b791d694
macarc <archiepiper@hotmail.com>
2021-02-14 11:42:14 +0000

    Add basic list diff

--- src/render/vdom.ts
+++ src/render/vdom.ts
@@ -53,7 +53,7 @@ function cache<Fn extends (...a: any) => VElement>(args: Parameters<Fn>, fn: Fn)
 }
 
 function patchNew(v: VElement) {
-  console.log('patch replacing')
+  console.log('creating element', v.name)
   const newElement = document.createElement(v.name);
 
   for (const attr in v.attrs) {
@@ -86,24 +86,108 @@ function patchNew(v: VElement) {
   return newElement;
 }
 
+const enum Change { Modified, Removed, Added }
+
+interface Diff<A> {
+  index: number,
+  change: Change,
+  newValue: A
+}
+
+function diffuneven<A>(longer: A[], shorter: A[], addChange: Change, removeChange: Change): Diff<A>[] {
+  let lengthDiff = longer.length - shorter.length;
+  let elementsAdded = [];
+  let so = 0; // shorter indexing offset
+  for (let i=0; i < longer.length; i++) {
+    if (longer[i] !== shorter[i + so]) {
+      if (longer[i] === shorter[i + so + 1]) {
+        const diff = {
+          index: i,
+          change: removeChange,
+          newValue: shorter[i + so]
+        }
+        elementsAdded.push(diff);
+        lengthDiff += 1;
+        so += 1;
+      } else {
+        let elementsBetween = [];
+        let isAddedElements = false;
+        let j = 0;
+        for (; j <= lengthDiff; j++) {
+          if (longer[i + j] === shorter[i + so]) {
+            isAddedElements = true;
+            break;
+          } else {
+            const diff = {
+              index: i + j, //index i because it will be inserted at index i
+              change: removeChange,
+              newValue: longer[i + j]
+            }
+            elementsBetween.push(diff);
+          }
+        }
+
+        if (isAddedElements) {
+          i += j;
+          elementsAdded.push(...elementsBetween);
+          lengthDiff -= j;
+          so -= j;
+        } else {
+          console.log(shorter[i + so], longer[i]);
+          const diff = {
+            index: i,
+            change: Change.Modified,
+            newValue: longer[i]
+          }
+          elementsAdded.push(diff);
+        }
+      }
+    }
+  }
+
+  return elementsAdded;
+
+}
+
+// the indexes it returns are intended to be used from right to left, insert/deleting along the way
+// TODO this is currently not the case for when before.length > after.length
+function difflist<A>(before: A[], after: A[]): Diff<A>[] {
+  if (before.length > after.length) {
+    return diffuneven(before, after, Change.Added, Change.Removed);
+  } else if (after.length > before.length) {
+    return diffuneven(after, before, Change.Removed, Change.Added);
+  } else {
+    // this doesn't do anything fancy like checking if elements are swapped, but actually that will not happen very much
+    const changes = [];
+    for (let i = 0; i < after.length; i++) {
+      if (after[i] !== before[i]) {
+        const diff = {
+          index: i,
+          change: Change.Modified,
+          newValue: after[i]
+        }
+        changes.push(diff);
+      }
+    }
+    return changes;
+  }
+}
+
 function patch(before: VElement, after: VElement) {
   if (before.node === null) {
     return patchNew(after);
   }
-  console.log('patch not replacing')
   for (const attr in after.attrs) {
     if (before.attrs[attr] !== after.attrs[attr]) {
       before.node.setAttribute(attr, after.attrs[attr]);
     }
   }
-
   for (const event in after.events) {
     if (before.node) {
       before.node.removeEventListener(event,before.events[event])
     }
     before.node.addEventListener(event, after.events[event]);
   }
-
   for (const child in after.children) {
     const aft = after.children[child];
     const bef = before.children[child];
@@ -155,6 +239,7 @@ function Note(c: string) {
 }
 
 function Score(counter: number): V {
+  //return h('div', { id: 'score' }, { mousedown: () => dispatch(counter) }, ['hello, world!', Note(counter.toString())]);
   return h('div', { id: 'score' }, { mousedown: () => dispatch(counter) }, ['hello, world!', (Math.random() < 0.5 ? h('p', {}, {}, ['hi']) : null), Note(counter.toString())]);
 }
 
