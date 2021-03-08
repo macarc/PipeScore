ead49c0c4d03314b8afd0f5a8c87db1e09828dc8
macarc <archiepiper@hotmail.com>
2021-03-08 14:51:29 +0000

    Improve auth interface, improve login page

--- src/Login/login.ts
+++ src/Login/login.ts
@@ -8,11 +8,13 @@ auth.listen(user => {
   if (user) window.location.replace('/scores')
 });
 
+
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementsByTagName('form')[0].addEventListener('submit', e => {
+  const loginForm = document.getElementById('login');
+  if (loginForm && loginForm instanceof HTMLFormElement) loginForm.addEventListener('submit', e => {
     e.preventDefault();
-    const emailEl = document.querySelector('input[type="email"]');
-    const passwdEl = document.querySelector('input[type="password"]');
+    const emailEl = loginForm.querySelector('input[type="email"]');
+    const passwdEl = loginForm.querySelector('input[type="password"]');
     if (emailEl && passwdEl && emailEl instanceof HTMLInputElement && passwdEl instanceof HTMLInputElement) {
       const email = emailEl.value;
       const passwd = passwdEl.value;
@@ -20,6 +22,26 @@ document.addEventListener('DOMContentLoaded', () => {
         alert('Please enter email and password');
       } else {
         auth.signIn(email, passwd)
+          .catch(() => alert('Invalid username or password'));
+      }
+    }
+  });
+  const signUpForm = document.getElementById('signup');
+  if (signUpForm && signUpForm instanceof HTMLFormElement) signUpForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const emailEl = signUpForm.querySelector('input[type="email"]');
+    const passwdEl = signUpForm.querySelector('#first-pwd');
+    const passwdRepeatEl = signUpForm.querySelector('#second-pwd');
+    if (emailEl && passwdEl && passwdRepeatEl && emailEl instanceof HTMLInputElement && passwdEl instanceof HTMLInputElement && passwdRepeatEl instanceof HTMLInputElement) {
+      const email = emailEl.value;
+      const passwd = passwdEl.value;
+      const passwd2 = passwdRepeatEl.value;
+      if (passwd !== passwd2) {
+        alert('Passwords do not match!');
+      } else if (!email || !passwd) {
+        alert('Please enter email and password');
+      } else {
+        auth.signUp(email, passwd)
           .catch(() => alert('Invalid username or password'));
       }
     }
--- src/firebase-auth.d.ts
+++ src/firebase-auth.d.ts
@@ -1,17 +1,95 @@
 declare module 'firebase-auth-lite' {
-  class User {
+
+  export type ProviderUser = {
     readonly email: string
+    readonly federatedId: string
+    readonly providerId: string
+    readonly rawId: string
+  }
+  export type TokenManager = {
+    readonly displayName: string
+    readonly email: string
+    readonly expiresIn: string
+    readonly idToken: string
+    readonly kind: string
     readonly localId: string
+    readonly refreshToken: string
+    readonly registered: boolean
+  } | {
+    readonly expiresAt: number
+    readonly idToken: string
+    readonly refreshToken: string
+  }
+  export type User = {
+    readonly createdAt: string
+    readonly email: string
+    readonly emailVerified: boolean
+    readonly lastLoginAt: string
+    readonly lastRefreshAt: string
+    readonly localId: string
+    readonly passwordHash: string
+    readonly passwordUpdatedAt: number
+    readonly providerUserData: ProviderUser[]
+    readonly tokenManager: TokenManager
+    readonly validSince: string
+  }
+
+  export type ProvidersForEmailResponse = {
+    readonly allProviders: string[]
+    readonly registered: boolean
+    readonly sessionId: string
+    readonly signInMethods: string[]
+  }
+
+  export interface ProviderOptions {
+    readonly name: string
+
+    readonly scope?: string
+  }
+
+  export interface OAuthFlowOptions {
+    readonly provider: string
+
+    readonly context?: string
+    readonly linkAccount?: boolean
+  }
+
+  export interface ProviderSignInOptions {
+    provider: string
+
+    context?: string
+    linkAccount?: boolean
+    oauthScope?: string
+  }
+
+  export interface AuthOptions {
+    apiKey: string
+    redirectUrl?: string
   }
 
   export default class Auth {
-    constructor(options: { apiKey: string, redirectUrl?: string })
+    constructor(options: AuthOptions)
 
-    signUp(email: string, password: string): Promise<void>
+    signUp(email: string, password: string): Promise<User>
+    signUp(): Promise<User>
     signIn(email: string, password: string): Promise<void>
-    signOut(): Promise<void>
+    signInWithCustomToken(tkoen: string): Promise<User>
+    signInWithProvider(options: ProviderSignInOptions | 'string'): never
+    signOut(): Promise<null>
 
-    authorizedRequest(input: RequestInfo, init?: RequestInit): Promise<Response>
+    handleSignInRedirect(): void
+
+    resetPassword(oobCode: string, newPassword: string): Promise<string>
+    deleteAccount(): Promise<void>
+
+    sendOobCode(requestType: 'PASSWORD_RESET', email: string): void
+    sendOobCode(requestType: 'EMAIL_SIGNIN' | 'VERIFY_EMAIL'): void
+
+    fetchProvidersForEmail(email: string): ProvidersForEmailResponse
+    fetchProfile(tokenManager?: object): Promise<User>
+    updateProfile(newData: User): Promise<User>
+
     listen(callback: (user: User) => void): () => void;
+    authorizedRequest(resource: Request | RequestInfo, init: RequestInit): Promise<Response>
   }
 }
