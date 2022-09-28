84aa8c50125cd5398b3cdff251e1394494ce70c8
macarc <archiepiper@hotmail.com>
2022-09-28 16:19:36 +0100

    Update dependency versions

--- src/PipeScore/State.ts
+++ src/PipeScore/State.ts
@@ -6,7 +6,6 @@
 import { Demo } from './DemoNote';
 import { PlaybackState } from './Playback';
 import { Menu } from './UI/model';
-import m from 'mithril';
 import { Score } from './Score';
 import { Selection } from './Selection';
 import { Note, Triplet } from './Note';
