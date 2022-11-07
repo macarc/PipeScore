0c490a8ec2941fc20e653c116938f5f99d1999a2
macarc <archiepiper@hotmail.com>
2022-11-07 19:59:46 +0000

    Format

--- server.py
+++ server.py
@@ -6,28 +6,31 @@
 from http.server import *
 import re
 
+
 class RequestHandler(SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs, directory='public')
-    
+        super().__init__(*args, **kwargs, directory="public")
+
     def end_headers(self):
-        self.send_header('Cache-Control', 'no-cache, must-revalidate')
+        self.send_header("Cache-Control", "no-cache, must-revalidate")
         super().end_headers()
-    
+
     def do_GET(self):
-        if self.path.startswith('/pipescore'):
+        if self.path.startswith("/pipescore"):
             self.path = "/pipescore.html"
-        elif self.path == '/':
-            self.path = '/index.html'
-        elif '.' not in re.search("([^/]*)$", self.path).group(0):
-            self.path += '.html'
+        elif self.path == "/":
+            self.path = "/index.html"
+        elif "." not in re.search("([^/]*)$", self.path).group(0):
+            self.path += ".html"
         return super().do_GET()
 
+
 def run():
-    addr = ('', 5000)
+    addr = ("", 5000)
     httpd = HTTPServer(addr, RequestHandler)
-    print('Serving on localhost:5000')
+    print("Serving on localhost:5000")
     httpd.serve_forever()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run()
