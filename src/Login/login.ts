fdc4e8885786cd3f7ec40971083e588cae143785
macarc <archiepiper@hotmail.com>
2023-01-20 12:52:43 +0000

    Remove date from copyright

--- src/Login/login.ts
+++ src/Login/login.ts
@@ -1,5 +1,5 @@
 //  PipeScore - online bagpipe notation
-//  Copyright (C) 2022 macarc
+//  Copyright (C) macarc
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
