macarc/PipeScore

commit 3fb34e48eda5447deba4953a61cb5d73b295a6f7

Bar: clean up width calculation slightly

src/PipeScore/Bar/index.ts
@@ -18,7 +18,7 @@ import { TimeSignature } from '../TimeSignature';
 import { NoteProps, Note, Triplet, lastNote } from '../Note';
 import { BaseNote } from '../Note/base';
 import { genId, ID, Item } from '../global/id';
-import { last, nlast } from '../global/utils';
+import { clamp, last, nlast } from '../global/utils';
 import width from '../global/width';
 import { Pitch } from '../global/pitch';
 import { NoteState } from '../Note/state';
@@ -307,19 +307,20 @@ export class Bar extends Item implements Previews<Note> {
     }
   }
   public anacrusisWidth(previousBar: Bar | null) {
-    if (this.isAnacrusis) {
-      if (this.fixedWidth !== 'auto') return this.fixedWidth;
-      const previousPitch = previousBar && previousBar.lastPitch();
-      const { total } = this.noteOffsets(previousPitch, this.nonPreviewNotes());
-      const previousTimeSignature = previousBar && previousBar.timeSignature();
-      const drawTimeSignature =
-        previousTimeSignature && !this.ts.equals(previousTimeSignature);
-      return Math.max(
-        width.reify(total, 5) + (drawTimeSignature ? 0 : this.ts.width()),
-        60
-      );
-    }
-    return 0;
+    if (this.fixedWidth !== 'auto') return this.fixedWidth;
+    return this.minWidth(previousBar);
+  }
+  public minWidth(previousBar: Bar | null) {
+    const previousPitch = previousBar && previousBar.lastPitch();
+    const { total } = this.noteOffsets(previousPitch, this.nonPreviewNotes());
+    const previousTimeSignature = previousBar && previousBar.timeSignature();
+    const drawTimeSignature =
+      previousTimeSignature && !this.ts.equals(previousTimeSignature);
+    return clamp(
+      width.reify(total, 5) + (drawTimeSignature ? 0 : this.ts.width()),
+      15,
+      60
+    );
   }
   // Returns an array where the nth item is the offset of the nth note
   // from the start of the bar.

src/PipeScore/Stave/index.ts
@@ -142,26 +142,26 @@ export class Stave {
       (width, bar, i) => width + bar.anacrusisWidth(previousBar(i)),
       0
     );
+    const widthAvailable = staveWidth - trebleClefWidth - totalAnacrusisWidth;
     const averageBarWidth =
-      (staveWidth - trebleClefWidth - totalAnacrusisWidth) /
-      (this._bars.length - anacruses.length);
-
-    const widths: number[] = [];
+      widthAvailable / (this._bars.length - anacruses.length);
     let extraWidth = 0;
 
-    this._bars.forEach((bar, i) => {
+    return this._bars.map((bar, i) => {
       if (bar.isAnacrusis) {
-        widths.push(bar.anacrusisWidth(previousBar(i)));
+        return bar.anacrusisWidth(previousBar(i));
       } else if (bar.fixedWidth !== 'auto') {
-        widths.push(bar.fixedWidth);
         extraWidth += bar.fixedWidth - averageBarWidth;
+        return bar.fixedWidth;
       } else {
-        widths.push(averageBarWidth - extraWidth);
+        const width = averageBarWidth - extraWidth;
+        if (width <= 0) {
+          console.error(`Bar has width ${width}!`);
+        }
         extraWidth = 0;
+        return width;
       }
     });
-
-    return widths;
   }
   public renderTrebleClef(x: number, y: number) {
     const scale = 0.00034 * settings.lineHeightOf(5);

src/PipeScore/global/utils.ts
@@ -37,6 +37,10 @@ export const last = <T>(array: T[]): T | null =>
 export const nfirst = <T>(array: T[]): T => array[0];
 export const nlast = <T>(array: T[]): T => array[array.length - 1];
 
+export function clamp(value: number, min: number, max: number): number {
+  return Math.min(max, Math.max(min, value));
+}
+
 // Monad :)
 export const nmap = <T, U>(a: T | null, fn: (b: T) => U): U | null => {
   if (a) return fn(a);

commit 415bed186a08dc7eecc2aa3f34bf4e309a1f875e

Bar: fix bug in anacrusis width determination
Introduced in https://github.com/macarc/PipeScore/commit/3fb34e48eda5447deba4953a61cb5d73b295a6f7

src/PipeScore/Bar/index.ts
@@ -317,9 +317,8 @@ export class Bar extends Item implements Previews<Note> {
     const previousTimeSignature = previousBar && previousBar.timeSignature();
     const drawTimeSignature =
       previousTimeSignature && !this.ts.equals(previousTimeSignature);
-    return clamp(
+    return Math.max(
       width.reify(total, 5) + (drawTimeSignature ? 0 : this.ts.width()),
-      15,
       60
     );
   }