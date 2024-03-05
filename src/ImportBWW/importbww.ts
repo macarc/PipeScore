dbad73dcca4f8006b57f731d373e2f42b48d4678
macarc <archiepiper@hotmail.com>
2024-03-05 00:39:01 +0000

    ImportBWW: use correct gap before first stave

--- src/ImportBWW/importbww.ts
+++ src/ImportBWW/importbww.ts
@@ -85,6 +85,7 @@ async function importfile(e: SubmitEvent) {
       )?.value;
 
       const initialTopOffset = 180;
+      score._staves[0].gap = initialTopOffset;
       score.name = scoreName;
       score.textBoxes[0].texts = [];
       score.textBoxes[0].texts.push({
